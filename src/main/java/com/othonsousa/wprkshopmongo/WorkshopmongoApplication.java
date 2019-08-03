fe6f6d3102cd55fdafcaff63b9b69a37db98a12b
Othon-Code <othonn.ssousa@gmail.com>
2019-08-03 09:22:36 -0300

    Priemiro commit-projeto Local

---
+++ src/main/java/com/othonsousa/wprkshopmongo/WorkshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.othonsousa.wprkshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoApplication.class, args);
+	}
+
+}
